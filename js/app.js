91fdad5780513f30f56c5d45b71bcb622e670d2d
clockwork189 <charles.desouza3000@gmail.com>
2012-06-30 11:39:49 -0400

    Rechanged structure

--- js/app.js
+++ js/app.js
@@ -1,80 +1,54 @@
-/**************************************************************************/
-/*************************Backbone Models**********************************/
-/**************************************************************************/
-
-/////////////////////////List Item Model///////////////////////////////////
-var ListItem = Backbone.Model.extend({
-	defaults : {	
-		"title": "",
-		"status": "Active",
-		"duedate": new Date()
+var TodoModel = Backbone.Model.extend({
+	defaults: function() {
+		return({
+			name: "Gotta get it done...",
+			dateCreated: new Date()
+		})
 	},
 	initialize: function() {
-
-	}
-});
-
-//////////////////////////List Model///////////////////////////////////////
-var List = Backbone.Model.extend({
-	defaults : {	
-		"title": ""
+		
 	},
-	initialize: function() {
-
-	}
 });
 
-/**************************************************************************/
-/*************************Backbone Collections*****************************/
-/**************************************************************************/
-
-//////////////////////////List Collection///////////////////////////////////
-var TodoList = Backbone.Collection.extend({
-	model: ListItem,
-	initialize: function(models, options) {
-		options || (options = {});
-		if(options.title) {
-				this.title = options.title;
-		}
-	}
-});
-
-
-/**************************************************************************/
-/*************************Backbone Views***********************************/
-/**************************************************************************/
-
-//////////////////////////List View///////////////////////////////////////
-var NewListView = Backbone.View.extend({
+var TodoView = Backbone.View.extend({
+	tagname: 'div',
+	id: 'todo_item',
+	className: 'todo_items',
+	
 	initialize: function() {
 		this.render();
 	},
+	
+	template: _.template('<h3><%= name %></h3>'),
+	
 	render: function() {
-		var variables = {label : "Enter a name for the list"};
-		var template = _.template($("#newListTemplate").html(), variables);
-		$(this.el).html(template);
+		var attributes = this.model.toJSON();
+		this.$el.html(this.template(attributes));
 	}
 });
 
-//////////////////////Show All List View///////////////////////////////////////
-var ShowAllListView = Backbone.View.extend({
-	initialize: function() {
-		this.render();
-	},
-	render: function() {
-		var variables = {title : "Enter a name for the list"};
-		var template = _.template($("#allListsTemplate").html(), variables);
-		$(this.el).html(template);
-	}
-});
+var AddNewItem = Backbone.View.extend({
+	el: $("#todoApp"),
 
-//////////////////////////List Item View///////////////////////////////////
-var NewListItemView = Backbone.View.extend({
+	template: _.template($("#newListTemplate").html()),
+
+	events: {
+		"click #newList": "addNewItem"
+	},
+
+	addNewItem: function() {
+		$("#addListDiv").html(this.template);
+	},
+
 	initialize: function() {
 		this.render();
 	},
+
 	render: function() {
-		var template = _.template($("#newListTemplate").html(), variables);
-		$(this.el).html(template);
-	}
+		//this.$el.append(this.template);
+	}	
 });
+var newItem = new AddNewItem;
+
+//var todomodel = new TodoModel;
+//var todo = new TodoView({model:todomodel });
--- js/functions.js
+++
@@ -1,9 +0,0 @@
-function AddList() {
-	var list_item_view = new NewListView({ el: $("#addListDiv")});
-}
-
-function CreateNewList() {
-	var name = $("#ListName").val();
-	var list = new TodoList({title: name});	
-	var all_list_view = new ShowAllListView({ el: $("#viewListDiv")})
-}
