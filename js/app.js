89c4482622a9c400e99f415b9ea7121d5fdab516
clockwork189 <charles.desouza3000@gmail.com>
2012-07-02 16:02:20 -0400

    Finally got a minor working model

--- js/app.js
+++ js/app.js
@@ -1,54 +1,123 @@
-var TodoModel = Backbone.Model.extend({
-	defaults: function() {
-		return({
-			name: "Gotta get it done...",
-			dateCreated: new Date()
-		})
-	},
-	initialize: function() {
+var TodoRouter = new (Backbone.Router.extend({
+	routes: {
+		"": "index",
+		"item/:id": "ShowTodoItem",
+		"item/edit/:id": "EditTodoItem",
+		"item/remove/:id": "RemoveTodoItem",
 		
+		"list/edit/:id": "EditTodoList",
+		"list/remove/:id": "RemoveTodoList",
+		"list/:id": "ShowTodoList"
 	},
-});
+	initalize: function(){
 
-var TodoView = Backbone.View.extend({
-	tagname: 'div',
-	id: 'todo_item',
-	className: 'todo_items',
-	
-	initialize: function() {
-		this.render();
 	},
-	
-	template: _.template('<h3><%= name %></h3>'),
-	
-	render: function() {
-		var attributes = this.model.toJSON();
-		this.$el.html(this.template(attributes));
+	ShowTodoItem: function(id) {
+		alert("show item " + id);		
+	},
+	ShowTodoList: function(id) {
+		alert("show list " + id);		
+	},
+	EditTodoItem: function(id) {
+		alert("edit item " + id);		
+	},
+	EditTodoList: function(id) {
+		alert("edit list " + id);		
+	},
+	AddTodoItem: function() {
+		//e.preventDefault();
+	},
+	AddTodoList: function(e) {
+		var newList = new TodoNewListView;
+		alert("add new list");
+	},
+	RemoveTodoItem: function(id) {
+		alert("remove item " + id);				
+	},
+	RemoveTodoList: function(id) {
+		alert("remove list " + id);				
+	},
+	Start: function(){
+		Backbone.history.start({ pushState: true, root: "/playground/todo/"});
+	}, 
+	index: function(){
+		//var todoListView = new TodoListView({ collection: TodoItemCollection });
+	}
+}));
+
+var TodoListModel = Backbone.Model.extend({
+	defaults: function() {
+		return {
+			title: "List Name",
+			description: "List Description",
+			dateCreated: new Date(),
+			isComplete: false
+		}
 	}
 });
 
-var AddNewItem = Backbone.View.extend({
-	el: $("#todoApp"),
+var TodoItemModel = Backbone.Model.extend({
+	defaults: function() {
+		return {
+			title: "Item Name",
+			description: "Item Description",
+			dateCreated: new Date(),
+			isDone: false
+		}
+	}
+});
 
-	template: _.template($("#newListTemplate").html()),
+var TodoListCollection = Backbone.Collection.extend({
+	model: TodoListModel
+})
 
+var TodoItemCollection = Backbone.Collection.extend({
+	model: TodoItemModel
+})
+
+var TodoListView = Backbone.View.extend({
+	template: _.template($("#allListsTemplate").html()),
+	el: $("#viewListDiv"),
 	events: {
-		"click #newList": "addNewItem"
+		
 	},
-
-	addNewItem: function() {
-		$("#addListDiv").html(this.template);
-	},
-
 	initialize: function() {
 		this.render();
 	},
-
 	render: function() {
-		//this.$el.append(this.template);
-	}	
+		this.$el.append(this.template(this.model.toJSON()));
+	}
 });
-var newItem = new AddNewItem;
 
-//var todomodel = new TodoModel;
-//var todo = new TodoView({model:todomodel });
+var AppView = Backbone.View.extend({
+	el: "#todoApp",
+	events: {
+	"click #newList": "ShowCreateListForm",	
+	"click #submitlist": "CreateNewList",	
+	},
+	template: _.template($("#newListTemplate").html()),
+
+	initialize: function(){
+
+	},
+	ShowCreateListForm: function() {
+		$("#newList").hide();
+		$("#addListDiv").show();
+		$("#addListDiv").html(this.template);
+	},
+	CreateNewList: function() {
+		var listmodel = new TodoListModel({title: $("#newListTitle").val(), description: $("#newListDescription").val(), });
+		var listView = new TodoListView({ model: listmodel });
+		$("#addListDiv").hide();
+		$("#newList").show();
+	}
+});
+
+$(function() {
+	TodoRouter.Start();
+	var view = new AppView;		
+});
+
+
+
+
